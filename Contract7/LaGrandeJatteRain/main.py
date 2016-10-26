3e968265f5e0330adcc9b1c9c32420c72e5e24ae
Newtoto <andrewrfindlay@ntlworld.com>
2016-10-26 11:18:49 +0100

    Merged Kieran with main project

--- Contract7/LaGrandeJatteRain/main.py
+++ Contract7/LaGrandeJatteRain/main.py
@@ -1,35 +1,45 @@
-import pygame, sys, colorsys
+import random
+import pygame, sys
 from pygame.locals import *
 
-width = 750
-height = 501
+pygame.init()
+clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((width, height), 0, 32)  # Setting screen size
+ball_pos_x = []
+ball_pos_y = []
+ball_speed_x = []
+ball_speed_y = []
+ball_radius = []
+xPos = 0
+yPos = 0
+WIDTH = 750
+HEIGHT = 501
+ball_index = 0
+window = pygame.display.set_mode((WIDTH,HEIGHT),0,32)
+BLACK = (0,0,0)
+BLUE = (0,0,255,6)
+bg = pygame.image.load("Images/BaseImage.jpg")
+num_balls = 9
+num_balls -= 1
 
-# --- Defining loading functions ---
-
-# function to load image from path, remember to omit 'Images/' from path
-def imageload(path):
-    return pygame.image.load('Images/' + path)
-
-
-# function to display images
-def appear(image):
-    screen.blit(image, (0, 0), None, 0)
-
-
-# --- Loading in image layers and resizing ---
-
-# base layer
-baseImage = imageload('BaseImage.jpg')
-appear(baseImage)
-
+for ball_index in xrange(num_balls):
+    ball_radius.append(random.randrange(2, 5))
 
 while True:
-
+    window.blit(bg, (0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
-    pygame.display.update()
+    for ball_index in xrange(num_balls):
+        xPos += 6
+        yPos += 6
+        radius = ball_radius[ball_index]
+
+        window.blit(bg, (0, 0))
+        pygame.draw.circle(window, BLUE, (xPos, yPos), radius)
+        pygame.display.update()
+        clock.tick(120)
+
+
